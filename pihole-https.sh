3064a4918dd086238efe40ebdc9542a7053cbbbd
Luiz Bizzio <luizrbizzio@gmail.com>
2025-01-05 00:41:12 +0100

    Update pihole-https.sh

--- pihole-https.sh
+++ pihole-https.sh
@@ -140,5 +140,36 @@ fi
 
 
 echo -e "\033[34mConfiguration completed! Certificates are stored in /etc/ssl/mycerts/.\033[0m"
-echo -e "\033[34mTo install the certificate on your device, copy and use the following certificate:\033[0m"
-sudo cat /etc/ssl/mycerts/$HOSTNAME.crt
+
+# Copy the certificate to the web server directory
+WEB_DIR="/var/www/html"
+CERT_FILE="/etc/ssl/mycerts/$HOSTNAME.crt"
+
+if [ -d "$WEB_DIR" ]; then
+    sudo cp "$CERT_FILE" "$WEB_DIR/"
+    sudo chmod 644 "$WEB_DIR/$HOSTNAME.crt"
+    echo -e "\033[32mCertificate copied to $WEB_DIR and is accessible via URL.\033[0m"
+
+    # Display the clickable URL for the certificate
+    if command -v hostname -I &>/dev/null; then
+        IP_ADDRESS=$(hostname -I | awk '{print $1}')
+        CERT_URL="http://$IP_ADDRESS/$HOSTNAME.crt"
+        echo -e "\033[34mClick the following link to download the certificate:\033[0m"
+        echo -e "\033[4;32m$CERT_URL\033[0m"
+    else
+        echo -e "\033[33mUnable to determine the IP address. The certificate should be accessible from:\033[0m"
+        echo -e "\033[4;32mhttp://<your-server-ip>/$HOSTNAME.crt\033[0m"
+    fi
+else
+    echo -e "\033[31mError: Web directory $WEB_DIR does not exist. The certificate cannot be made available via URL.\033[0m"
+    echo -e "\033[34mCertificate available in text, copy and use the following content:\033[0m"
+    sudo cat "$CERT_FILE"
+fi
+
+echo -e "\033[34mInstall this certificate on your device as a Trusted Root Certificate Authority (CA).\033[0m"
+echo -e "\033[34mSteps:\033[0m"
+echo -e "\033[34m1. Download the certificate using the provided URL or copy the content manually to create a .crt file.\033[0m"
+echo -e "\033[34m2. Open your device's certificate manager (e.g., Windows, Linux, Android, macOS).\033[0m"
+echo -e "\033[34m3. Import the certificate into the 'Trusted Root Certification Authorities' store.\033[0m"
+echo -e "\033[34m4. Restart your browser or application to apply the changes.\033[0m"
+
